38f3df50e0024a7a0972ede7f28f092aa7bc8c2f
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-12-01 23:51:16 +0100

    Explain that servers might not let us connect becuase they're indexing

--- src/ui/WidgetConnection.vue
+++ src/ui/WidgetConnection.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseWidget link="" :status="connected ? 'good' : 'unsure'">
     Connection:
-    {{ connected ? 'Connected' : "Not connected. Can't find a scuttlebutt client on this machine to connect to. Please start one if you haven't." }}
+    {{ connected ? 'Connected' : "Not connected. Can't find a scuttlebutt client on this machine to connect to. Please start one if you haven't. Note that sometimes, clients that are busy indexing don't accept incoming connections." }}
   </BaseWidget>
 </template>
 
