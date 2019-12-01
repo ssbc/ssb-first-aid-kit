c309ecfb147503f7adbef1bca46846c067edcf85
Jonathan Dahan <hi@jonathan.is>
2019-12-01 13:56:44 -0500

    Hide the menu bar until alt is pressed

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    }
+    },
+    autoHideMenuBar: true
   })
 
   const appURL = process.env.NODE_ENV === 'development'
